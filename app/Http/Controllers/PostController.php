6926c358d6cb225263fecb6cb49475bd5f48bedd
Alessandra <maalmarinba@ittepic.edu.com>
2025-04-28 15:46:40 -0600

    Avance 28April2025

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,18 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 
 class PostController extends Controller
 {
+    use AuthorizesRequests;
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['show', 'index']);
     }
 
 
-    public function index(User $user)
+    public function index(User $user) //Traer los post asociados al usario que se visita
+    //Carga una vista específica y pasa las variables que esa vista va a necesitar para mostrar contenido dinámico.
     {
-        $posts = Post::where('user_id', $user->id)->get();
+        $posts = Post::where('user_id', $user->id)->paginate(20);
 
         //Enviando los datos a la vista
         return view('dashboard',[
@@ -60,6 +64,22 @@ class PostController extends Controller
         //     'imagen'=> $request->imagen,
         //     'user_id'=>auth()->user()->id
         // ]);
+
+        return redirect()->route('posts.index', auth()->user()->username);
+    }
+
+    public function show(User $user, Post $post){ //Muestra solamente un recurso
+        return view('posts.show',[
+            'post' => $post,
+            'user' => $user
+
+        ]);
+    }
+
+    public function destroy(Post $post)
+    {
+        $this->authorize('delete', $post);
+        $post->delete();
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ComentarioController;
 
 Route::get('/', function () {
     return view('principal');
@@ -19,4 +20,7 @@ Route::post('/logout',[LogoutController::class,'store'])->name('logout');
 Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
 Route::post('/posts',[PostController::class,'store'])->name('posts.store');
 Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
+Route::get('/{user:username}/post/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::post('/{user:username}/post/{post}', [ComentarioController::class, 'store'])->name('comentarios.store');
+Route::delete('/posts/{post}',[PostController::class, 'destroy'])->name('posts.destroy');
 Route::get('/{user:username}', [PostController::class,'index'])-> name('posts.index');
